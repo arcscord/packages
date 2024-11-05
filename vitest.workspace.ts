775b3fdb3355da71fc924ec90006c38c0d228802
Chris <christopher@arcoz.dev>
2024-11-05 22:40:05 +0100

    chore: remove arcscord vitest config from workspace definition

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,7 +1,6 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-  "./packages/arcscord/vitest.config.mts",
   "./packages/better_error/vitest.config.mts",
   "./packages/error/vitest.config.mts",
 ]);
